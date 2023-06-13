e4c0fd5b49dc45b0f407d379d7c58f19fbff6e7f
Heine Günther <heine_gunther@hotmail.com>
2023-06-13 13:51:21 +0200

    changed file structure

--- app/about/page.js
+++ app/about/page.js
@@ -1,13 +1,11 @@
-import Footer from "../components/footer";
-import Navbar from "../components/navbar";
+
 
 
 export default function Page() {
   return (
     <>
-    <Navbar />
+
     <h1>Hello, About!</h1>
-    <Footer />
     </>
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,15 +1,8 @@
-import Footer from "./components/footer";
-import Navbar from "./components/navbar";
-
-
 export default function Home() {
   return (
-    <>
-   <Navbar />
-   
+    <>   
    <h1>Hello, Home</h1>
    <p>afjkbnakdfjbnkafdjbndfbnd kjfbnakfj nafkjnafkjn kjfdnkajfdnvk ffkvjfkjndbkjafdnb fjbnldfbnldfkbn ldfjbnlafdbnlafdbnladfnb</p>
-   <Footer />
    </>
   )
 }
