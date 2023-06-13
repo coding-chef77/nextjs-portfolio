d4c43ed19555953820e15f0e904623bf16d9ba8b
Heine Günther <heine_gunther@hotmail.com>
2023-06-13 22:32:28 +0200

    started hero section

--- app/page.js
+++ app/page.js
@@ -1,8 +1,9 @@
+import Hero from "./components/hero";
+
 export default function Home() {
   return (
     <>   
-   <h1>Hello, Home</h1>
-   <p>afjkbnakdfjbnkafdjbndfbnd kjfbnakfj nafkjnafkjn kjfdnkajfdnvk ffkvjfkjndbkjafdnb fjbnldfbnldfkbn ldfjbnlafdbnlafdbnladfnb</p>
+  <Hero />
    </>
   )
 }
